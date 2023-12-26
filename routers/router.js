96154175fd128bdc418439c26d4becd2c8f9419d
George Bazerashvili <georgebazerashvili200@gmail.com>
2023-12-26 09:46:25 +0400

    configuration changed

--- routers/router.js
+++ routers/router.js
@@ -22,9 +22,3 @@ Router.get("/username", Authorization, getUserInfo);
 Router.get("/verify", Authorization, verifyToken);
 
 module.exports = Router;
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjY1NzllNTE4NDFkYjI2NjJmNTZhZmM5ZiIsImlhdCI6MTcwMzQ5Njg0NSwiZXhwIjoxNzAzNDk2ODQ2fQ.KkvYPUUAM6-e2Y1EqrbnoHrai_H9yCtJNpbLh4_ZkjE
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjY1NzllNTE4NDFkYjI2NjJmNTZhZmM5ZiIsImlhdCI6MTcwMzQ5Njg3MiwiZXhwIjoxNzAzNDk2ODgyfQ.zbf1J6Z3OsLa4etbOqZMADQsiaTEkHT-3t2KhIs2v38
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjY1NzllNTE4NDFkYjI2NjJmNTZhZmM5ZiIsImlhdCI6MTcwMzQ5NjkwNywiZXhwIjoxNzAzNDk2OTE3fQ.O_AD7wx0eK89MC6gWU2jNGkee__ya-KLZuH-yyGN5SE
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { createServer } = require("http");
 const server = createServer(app);
 
 const io = require("socket.io")(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://vanilla-chatapp.netlify.app" },
 });
 
 io.on("connection", (socket) => {
